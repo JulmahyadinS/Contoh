798d81736bc34a49fce426cbe2a8adfa95521475
JulmahyadinS <147465163+JulmahyadinS@users.noreply.github.com>
2023-10-10 12:56:58 +0800

    Add files via upload

---
+++ Fungsi.py
@@ -0,0 +1,8 @@
+# Definisikan array
+arr = [12, 45, 78, 23, 56, 89, 34]
+
+# Temukan nilai maksimum menggunakan fungsi max()
+nilai_maks = max(arr)
+
+# Cetak nilai maksimum
+print("Nilai maksimum dalam array adalah:", nilai_maks)
